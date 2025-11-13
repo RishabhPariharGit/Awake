0de5106a5ff5b21b4c627428de1cf7f0b5cb2e99
Rishabh Singh Parihar <your-email@example.com>
2025-11-13 16:09:00 +0530

    add new style

--- case-1/case-1.js
+++ case-1/case-1.js
@@ -1,9 +1,12 @@
 const heading = document.getElementById('MainHeading');
+const pageui = document.getElementById('main-ui-wrapper');
 
 heading.addEventListener('mouseover',()=>{
     heading.style.color = 'white';
+    pageui.style.backgroundColor = 'black'
 });
 
 heading.addEventListener('mouseout', ()=>{
     heading.style.color = 'black';
+    pageui.style.backgroundColor = '#2fc042'
 })
